8c70b675609b25c5825d8d8c0d588d26ea179664
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 17:34:18 +0000

    Initial Commit

---
+++ LazyGridsDemo/LazyGridsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridsDemoApp.swift
+//  LazyGridsDemo
+//
+//  Created by Azu on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
